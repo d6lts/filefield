eea7febf204d80f8c04c096852afa0de8cdbb122
Darrel O\'Pry <dopry@22202.no-reply.drupal.org>
2009-01-31 17:56:22 +0000

    #359056 by m29Corey, update jquery input type selector to no longer use @ to reference attributes.

--- filefield.js
+++ filefield.js
@@ -4,12 +4,12 @@
  * Auto-attach standard client side file input validation.
  */
 Drupal.behaviors.filefieldValidateAutoAttach = function(context) {
-  $("input[@type='file'][accept]", context).change( function() {
+  $("input[type='file'][accept]", context).change( function() {
     // Remove any previous errors.
     $('.file-upload-js-error').remove();
 
     /**
-     * Add client side validation for the input[@file] accept attribute.
+     * Add client side validation for the input[type=file] accept attribute.
      */
     var accept = this.accept.replace(/,\s*/g, '|');
     if (accept.length > 1) {
