9afe207a7b8030d3d85f9dab669a52e9c7a90f59
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2010-04-24 02:47:49 +0000

    #698196: Double negative "cannot not" in error message.

--- filefield.js
+++ filefield.js
@@ -15,7 +15,7 @@ Drupal.behaviors.filefieldValidateAutoAttach = function(context) {
     if (accept.length > 1 && this.value.length > 0) {
       var v = new RegExp('\\.(' + accept + ')$', 'gi');
       if (!v.test(this.value)) {
-        var error = Drupal.t("The selected file %filename cannot not be uploaded. Only files with the following extensions are allowed: %extensions.",
+        var error = Drupal.t("The selected file %filename cannot be uploaded. Only files with the following extensions are allowed: %extensions.",
           { '%filename' : this.value, '%extensions' : accept.replace(/\|/g, ', ') }
         );
         // What do I prepend this to?
